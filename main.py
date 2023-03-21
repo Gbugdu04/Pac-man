725fa250adb109e7aefe821ee829589b51421f1f
a1bderrahmane <124886327+a1bderrahmane@users.noreply.github.com>
2023-03-21 17:02:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
-import tkinter 
+import tkinter
+from PIL import Image,ImageTk
+import json
+
+class labyrinth:
+    def __init__(self) :
+        pass
+    
+    def create_matrix(self):
+        pass
+    def convert_to_lab(self):
+        pass
+    def draw_labyrinth(self):
+        pass
+
+
+
+class pacman:
+    def __init__(self):
+        pass
+    def load_images(self):
+        pass
+    def create_images(self):
+        pass
+    def move_pacman(self):
+        pass
+    def read_graph(self):
+        pass
+    def djiksrtra(self):
+        pass
+    def find_path(self):
+        pass
+    def follow_path(self):
+        pass
+    def update_path(self):
+        pass
+    def kill_pac(self);
+        pass
+    def pac_dead(self):
+        pass
+    def move_pac(self):
+        pass
+    
